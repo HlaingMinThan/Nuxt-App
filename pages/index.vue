cc9f424c355297cda155b6a35e572be60d373d2f
HlaingMinThan <hlaingminthan@92@gmail.com>
2022-08-05 14:03:55 +0700

    auto import Composables functions

--- pages/index.vue
+++ pages/index.vue
@@ -3,5 +3,11 @@
     <Title>Home</Title>
     <h1>home</h1>
     <Counter/>
+
+    <h1>x y mouse coordinates</h1>
+    <p>x-{{x}},y{{y}}</p>
   </div>
 </template>
+<script setup>
+  let {x,y}=useMouse()
+</script>
